7e9f2ec320fdf22a05842d70b1a29e5ba89a8134
KamalShah <shahdullaev@gmail.com>
2017-02-15 16:51:10 +0300

    Add files via upload

--- Guide.py
+++ Guide.py
@@ -1,4 +1,4 @@
-print("""Hello my little friend!
+print("""Hello my little friends!
 	Do you want to improve your knowledge?""")
 
 a = input("1 - Yes, 2 - No")
@@ -9,31 +9,62 @@ else:
 
 print("""What will we study?
 	You are right! We will study letters!
-	Let's go! Repeat after me:
-	1) A
-	2) B
-	3) C
-	4) D
-	5) E
-	6) F
-	7) G
-	8) H
-	9) I
-	10) J
-	11) K
-	12) L
-	13) M
-	14) N
-	15) O
-	16) P
-	17) Q
-	18) R
-	19) S
-	20) T 
-	21) U
-	22) V
-	23) W
-	24) X
-	25) Y
+		Let's go! Repeat after me:
+			1) A
+				2) B
+					3) C
+						4) D
+							5) E
+								6) F
+									7) G
+										8) H
+											9) I
+												10) J
+													11) K
+														12) L
+															13) M
+														14) N
+													15) O
+												16) P
+											17) Q
+										18) R
+									19) S
+								20) T 
+							21) U
+						22) V
+					23) W
+				24) X
+			25) Y
 	And the last letter is Z""")	
 input()
+
+print("""Okay, guys!
+	Our second lesson will about...
+		Yeap! About greeting and acquaintance!
+			What do we usually say, when you see your friends?
+				Of course, Hello!
+				 	What else?
+				 		OK, I am saying now:
+							How are you? and What will you do?
+									Let's repeat:
+									Hello!
+										How are you?
+											What will you do?
+												Nice to see you""")
+input()
+
+print("""Oh, I am sorry!
+			How could I not notice you? 
+				Okay, our third lesson will be about numerals!
+					I think that you know their!
+						Let's remember:
+						1 - One
+							2 - Two
+								3 - Three
+									4 - Four 
+										5 - Five
+											6 - Six
+												7 - Seven
+													8 - Eight
+														9 - Nine
+															10 -Ten""")
