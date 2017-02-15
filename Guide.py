2487fed63f3564870883645c960da6b606b0186e
KamalShah <shahdullaev@gmail.com>
2017-02-15 16:37:43 +0300

    Add files via upload

---
+++ Guide.py
@@ -0,0 +1,39 @@
+print("""Hello my little friend!
+	Do you want to improve your knowledge?""")
+
+a = input("1 - Yes, 2 - No")
+if a == "2":
+	print("I am sorry, but I must to say GOODBYE")
+else:
+	print("Let's start our first lesson!")	
+
+print("""What will we study?
+	You are right! We will study letters!
+	Let's go! Repeat after me:
+	1) A
+	2) B
+	3) C
+	4) D
+	5) E
+	6) F
+	7) G
+	8) H
+	9) I
+	10) J
+	11) K
+	12) L
+	13) M
+	14) N
+	15) O
+	16) P
+	17) Q
+	18) R
+	19) S
+	20) T 
+	21) U
+	22) V
+	23) W
+	24) X
+	25) Y
+	And the last letter is Z""")	
+input()
